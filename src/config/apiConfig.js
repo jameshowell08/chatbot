738e67524db3e0d2a163bfe0e2035c15e587e18e
jameshowell08 <howelljamescong@gmail.com>
2024-05-31 22:50:02 +0700

    DELETE COMMENTS

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,14 +1,3 @@
-//const apiKey = "AIzaSyC8h8c-A3zuFMNVH3rDFRvhw8IsRe1XqLY";
-
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- */
-
 import {
   GoogleGenerativeAI,
   HarmCategory,
